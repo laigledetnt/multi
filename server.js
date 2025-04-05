9cf70bd3d490779f81053be568b6d2b69a70e22e
laigledetnt <maelbrachet965@gmail.com>
2025-04-05 19:22:13 +0200

    modified:   public/index.html 	modified:   server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
   socket.emit('initialPositions', players);
 
   // Mettre à jour la position d'un joueur
-    socket.on('updatePosition', (data) => {
+  socket.on('updatePosition', (data) => {
     players[socket.id].position = data.position;
 
     // Envoyer la position mise à jour à tous les autres joueurs
@@ -29,14 +29,14 @@ io.on('connection', (socket) => {
       id: socket.id,
       position: data.position,
     });
-    });
+  });
 
   // Lorsqu'un joueur se déconnecte
-    socket.on('disconnect', () => {
+  socket.on('disconnect', () => {
     console.log('Un joueur est déconnecté : ' + socket.id);
     delete players[socket.id];
     socket.broadcast.emit('playerDisconnected', socket.id);
-    });
+  });
 });
 
 // Démarrer le serveur sur le port 3000
